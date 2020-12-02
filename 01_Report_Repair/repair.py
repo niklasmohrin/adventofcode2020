acdfde53a5a4982605545afbc2fd2a9d3dc32bfa
Niklas Mohrin <niklas.mohrin@gmail.com>
2020-12-02 18:56:36 +0100

    Add part one of day one back

--- 01_Report_Repair/repair.py
+++ 01_Report_Repair/repair.py
@@ -5,6 +5,12 @@ import sys
 filename = sys.argv[1] if len(sys.argv) == 2 else "input"
 data = set(map(int, open(filename).read().strip().split("\n")))
 
+# Part one
+for n in data:
+    if 2020 - n in data:
+        print(f"Found: {n} and {2020 - n} with product {n * (2020 - n)}")
+
+# Part two
 for n1 in data:
     for n2 in data:
         if 2020 - n1 - n2 in data:
